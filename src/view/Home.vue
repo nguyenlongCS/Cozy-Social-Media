35df71bd546e8fe3cc5b1348326476420e43506c
nguyenlongCS <thanhlong86.tt@gmail.com>
2025-07-14 22:41:32 +0700

    Version 1.0 - Giao diện Home

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -23,7 +23,7 @@ Logic quản lý ngôn ngữ (toggle giữa vi/en)
 import NavLeft from '@/components/NavLeft.vue'
 import NavMid from '@/components/NavMid.vue'
 import NavRight from '@/components/NavRight.vue'
-import Left from '@/components/Menu.vue'
+import Left from '@/components/Left.vue'
 import Main from '@/components/Main.vue'
 import Right from '@/components/Right.vue'
 import Footer from '@/components/Footer.vue'
@@ -51,32 +51,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.home {
-  width: 100%;
-  max-width: 79.0625rem;
-  height: 39.4375rem;
-  display: flex;
-  flex-direction: column;
-  background: #2B2D42;
-}
-
-.header {
-  width: 100%;
-  height: 3.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  box-shadow: 0 0.125rem 0.3125rem rgba(0, 0, 0, 0.2);
-  background: #2B2D42;
-}
-
-.body {
-  width: 100%;
-  height: 32.44rem;
-  display: flex;
-  justify-content: space-between;
-  background: #2B2D42;
-}
-</style>
--- src/view/Login.vue
+++ src/view/Login.vue
@@ -2,7 +2,7 @@
 View trang đăng nhập
 -->
 <template>
-  <<div class="login">
+  <div class="login">
     <div class="header">
       <NavLeft :currentLanguage="currentLanguage" />
       <NavMid />
@@ -26,8 +26,8 @@ export default {
     NavMid,
     NavRight,
     Footer
-},
-data() {
+  },
+  data() {
     return {
       currentLanguage: 'vi'
     }
@@ -39,32 +39,3 @@ data() {
   }
 }
 </script>
-
-<style scoped>
-.home {
-  width: 100%;
-  max-width: 79.0625rem;
-  height: 39.4375rem;
-  display: flex;
-  flex-direction: column;
-  background: #2B2D42;
-}
-
-.header {
-  width: 100%;
-  height: 3.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  box-shadow: 0 0.125rem 0.3125rem rgba(0, 0, 0, 0.2);
-  background: #2B2D42;
-}
-
-.body {
-  width: 100%;
-  height: 32.44rem;
-  display: flex;
-  justify-content: space-between;
-  background: #2B2D42;
-}
-</style>
